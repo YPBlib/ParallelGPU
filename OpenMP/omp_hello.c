5a9c4c6623e8b0a84b2850b56f545d70bb920ab6
YPBlib <ypb1024@foxmail.com>
2019-03-15 04:03:04 +0800

    修改了语法错误.pthread 的多线程只有1个pid且多线程占用1个核心,OpenMP 的多线程有多个pid,占用多个核心

--- OpenMP/omp_hello.c
+++ OpenMP/omp_hello.c
@@ -1,20 +1,29 @@
+
 //$ gcc -g -Wall -fopenmp -o omp_hello omp_hello.c
 //$ ./omp_hello 4
-#include<stdio.h>
-#include<stdlib.h>
-#include<omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <math.h>
 
 void Hello(void)
 {
     int local_rank=omp_get_thread_num();
     int thread_count = omp_get_num_threads();
     printf("Hello from thread %d of %d \n",local_rank,thread_count);
+    double i=0.6,j=2,k=5;
+    while(1)
+    {
+      i=sin(j)*200.4;
+      j=cos(k)*1000/45;
+      k=i*i*j;
+    }
 }
 
-int main(void)
+int main(int argc,char**argv)
 {   
     int thread_count = strtol(argv[1],NULL,10);
-#pragma omp parallel num_threads(thread_count);
+#pragma omp parallel num_threads(thread_count)
     Hello();
 
     return 0;
@@ -22,12 +31,39 @@ int main(void)
 
 
 
+/*
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
+
+void* f()
+{
+  double i=0.6,j=2,k=5;
+  while(1)
+  {
+    i=sin(j)*200.4;
+    j=cos(k)*1000/45;
+    k=i*i*j;
+  }
+}
 
 
 
 
+int main()
+{
+  int pt[36]={0};
+  pthread_t pd[36];
+  void *r;
+  for(int i=0;i<36;++i)
+  {
+    pt[i]=pthread_create(&pd[i],NULL,f,NULL);
+    pthread_join(pd[i],&r);
+  }
 
+  return 0;
+}
 
-
-
-
+*/
